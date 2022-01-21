e4f5a8f4edd04e09d86693d718a6dff9fbc08d7e
Jack DeVillier <jdevillier03@gmail.com>
2022-01-21 17:02:28 -0500

    tidying

--- wordle.js
+++ wordle.js
@@ -12,75 +12,70 @@ let file = fs.readFileSync("./answers.txt").toString();
 file = file.split("\n");
 let targetWord = file[Math.floor(Math.random() * file.length)];
 console.log(targetWord);
-// wordle(targetWord);
-let tempword = "thing"
-wordle(tempword);
-//O is gray, I is yellow, X is gree
+
+wordle(targetWord);
+
+//O is gray, I is yellow, X is green
 
 function wordle(answer) {
+
+    //the answerDupeCheck is a string with the same characters as the answer at the beginning of the guessing process, but every time a "hint" is given, (i.e. the letter is right (X), the letter is somewhere else in the word (I)) that letter is removed from answerDupeCheck. answerDupeCheck is used during the I phase to make sure the amount of hints given is never higher than the frequency of a letter.
+
+
     let answerDupeCheck = answer;
+
+    //just to save time as well as space, I set all of the 
+    //values initially to O, or incorrect
+
     let accuracyArray = ["O", "O", "O", "O", "O"]
     rl.question("Guess a five letter word\n", function(guess) {
+
+        //the word must be present in the ~5000 5-letter words I pulled from another github repo
         if (wordBank.join(' ').includes(guess)) {
 
             guess = guess.toLowerCase();
-            // for (let letter = 0; letter < guess.length; letter++) {
-            //     //check the same letter pos in guess and answer
-            //     if (answer[letter] == guess[letter])
-            //     //they are the same, add X to the accuracy array, placing it in the first 
-            //         accuracyArray.push("X");
-            //         answerDupeCheck = removeLetterFromDupe(answer,answer[letter])
-            //     //check the single guess pos with every pos in the answer
-            //     else if (answer[letter] != guess[letter]) {
-            //         let count;
-            //          for (let answerLetter = 0; answerLetter < answer.length; answerLetter++) {
-            //             if (answer[answerLetter] == guess[letter])
-            //                 count = 1;
-            //         }
-            //         if (count == 1) {
-            //             //the guess letter is in the answer but not in the same pos
-            //             accuracyArray.push("I");
-            //         } else {
-            //             //the letter is not in the answer anywhere
-            //             accuracyArray.push("O");
-            //         }
-            //     }
 
-            // }
+
+            //X phase
+
             for (let letter = 0; letter < guess.length; letter++) {
+
                 //check the same letter pos in guess and answer
+
                 if (answer[letter] == guess[letter]) {
+
                     //they are the same, add X to the accuracy array, placing it in the first 
+                    
                     accuracyArray[letter] = "X";
-                    answerDupeCheck = removeLetterFromDupe(answer[letter],answerDupeCheck);
+                    answerDupeCheck = removeLetterFromDupe(guess[letter], answerDupeCheck);
                 }
             }
 
-
-
-
-
-
-
-
-
+            //I phase
 
             for (let letter = 0; letter < guess.length; letter++) {
                 console.log(answer[letter] + ", " + guess[letter]);
+
+                //the guess letter is in the answer but not in the same pos
+
                 if (answer[letter] != guess[letter] && answer.includes(guess[letter]) && answerDupeCheck.includes(guess[letter])) {
                     accuracyArray[letter] = "I";
-                    answerDupeCheck = removeLetterFromDupe(guess[letter],answerDupeCheck);
+                    answerDupeCheck = removeLetterFromDupe(guess[letter], answerDupeCheck);
                 }
             }
             console.log(accuracyArray);
+
             //win case
+
             if (accuracyArray.join("") == "XXXXX") {
                 console.log("You win!!");
             } else {
                 wordle(answer);
             }
         } else {
+
             //if the word does not exist
+
             console.log("It needs to be a real word!");
             wordle(answer);
         }
