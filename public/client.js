0861b1767b1f3247af5d606e5b5df9bb78495d1f
Jack DeVillier <jdevillier03@gmail.com>
2022-02-07 22:34:33 -0500

    functional user interface

--- public/client.js
+++ public/client.js
@@ -1,9 +1,13 @@
 let currLetter = 1;
-while (true) {
-$(document).keypress(function(event) {
-	if (event.which > 64 && event.which < 91) {
-		console.log(event.which);
-		$("#" + currLetter).append(fromCharCode(event.which) + "");
-	}
+$(document).keyup(function(event) {
+    if (event.which > 64 && event.which < 91 && currLetter < 6) {
+        $("#" + currLetter).append(String.fromCharCode(event.which) + "");
+        if (currLetter < 5)
+            currLetter++;
+    }
+    if (event.which == 8 && currLetter > 0) {
+        $("#" + currLetter).contents().remove();
+        if (currLetter > 1)
+            currLetter--;
+    }
 });
-}
